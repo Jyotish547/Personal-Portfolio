4946775f27a1bda565d48c586b372e1ec528d5cc
Jyotish547 <jyotish547@gmail.com>
2023-10-25 04:03:24 -0600

    Resume Section Completed - Home Page

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import '../public/output.css';
 
 import '@fortawesome/fontawesome-free/css/all.css';
 
+import imgUrl from '../assets/about.png';
+
+let img = document.getElementById("about-img") as HTMLImageElement;
+
+img.src = imgUrl;
+
 console.log("Bolla");
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/index.html"],
     theme: {
-      extend: {},
+      extend: {
+      boxShadow: {
+        'custom': '0 0 30px -10px rgba(0, 0, 0, 0.7)',
+      }
+    },
     },
     plugins: [],
   }
